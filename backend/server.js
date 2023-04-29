53ab555bc26e7ece388eb5c2caa20c5838ea8dbb
gino <georgino197@gmail.com>
2023-04-29 21:08:18 +0200

    add route server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
 const port = process.env.PORT || 5000;
 const connectDB = require('./config/db');
 
@@ -9,5 +10,11 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/goals', require('./routes/goalRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
+
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
