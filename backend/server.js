e2cc92efd7f6ae228334d520634b8ffff1d47a24
gino <georgino197@gmail.com>
2023-04-29 20:37:21 +0200

    create model

--- backend/server.js
+++ backend/server.js
@@ -7,5 +7,7 @@ const connectDB = require('./config/db');
 connectDB();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
