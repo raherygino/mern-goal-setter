f1362bce75afaa452c2aa8e8a7f14e64b7294f12
gino <georgino197@gmail.com>
2023-04-30 10:29:58 +0200

    Ajust server

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose')
 
-const connectDB = () => {
-    mongoose
-      .connect(process.env.MONGO_URI, { useNewUrlParser: true })
-      .then(() => console. log('\x1b[44m Database connected successfully \x1b[0m'))
-      .catch((err) => console.log(err));
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI)
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
 }
 
 module.exports = connectDB
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,39 @@
-
+const path = require('path');
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
-const port = process.env.PORT || 5000;
 const connectDB = require('./config/db');
+const port = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+  });
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
 
 app.use(errorHandler);
 
