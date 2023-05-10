24113b63dae68c626692ccfea5fc8bdcda6ac64d
raherygino <georgino197@gmail.com>
2023-05-10 18:46:37 +0200

    Service & slice todo

---
+++ frontend/src/features/todos/todoService.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+const API_URL = 'http://127.0.0.1:5000/api/todos/'
+
+// Create new todo
+const createTodo = async (todoData, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }
+  const response = await axios.post(API_URL, todoData, config)
+  return response.data
+}
+
+const getTodos = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    }
+  }
+
+  const response = await axios.get(API_URL, config)
+
+  return response.data
+}
+
+
+const todoService = {
+    createTodo,
+    getTodos
+}
+
+export default todoService
---
+++ frontend/src/features/todos/todoSlice.js
@@ -0,0 +1,93 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import todoService from "./todoService"
+
+const initialState = {
+    todos: [],
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: '',
+}
+
+export const createTodo = createAsyncThunk(
+    'todos/create',
+    async (todoData, thunkAPI) => {
+        try {
+            const token = thunkAPI.getState().auth.user.token
+            return await todoService.createTodo(todoData, token)
+        } catch (error) {
+            const message = 
+            (error.response &&
+                error.response.data &&
+                error.response.data.message) ||
+            error.message ||
+            error.toString()
+            return thunkAPI.rejectWithValue(message)
+        }
+    }
+)
+
+export const getTodos = createAsyncThunk(
+    'todos/getAll',
+    async (_, thunkAPI) => {
+        try {
+            const token = thunkAPI.getState().auth.user.token
+            return await todoService.getTodos(token)
+        } catch (error) {
+            const message = 
+            (error.response &&
+                error.response.data &&
+                error.response.data.message) ||
+            error.message ||
+            error.toString()
+            return thunkAPI.rejectWithValue(message)
+        }
+    }
+)
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        reset: (state) => initialState,
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(createTodo.pending, (state) => {
+                state.isLoading = true
+            })
+
+            .addCase(createTodo.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.todos.push(action.payload)
+            })
+
+            .addCase(createTodo.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+            })
+
+
+
+            .addCase(getTodos.pending, (state) => {
+                state.isLoading = true
+            })
+
+            .addCase(getTodos.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.todos = action.payload
+            })
+
+            .addCase(getTodos.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+            })
+    }
+})
+
+export const { reset } = todoSlice.actions
+export default todoSlice.reducer
