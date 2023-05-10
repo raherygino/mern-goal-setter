64c190c00acb7aa9fc1a65bad45887d62483a3b2
raherygino <georgino197@gmail.com>
2023-05-10 18:47:04 +0200

    Setup reducers

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authSlice'
 import goalReducer from '../features/goals/goalSlice'
+import todoReducer from '../features/todos/todoSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     goals: goalReducer,
+    todos: todoReducer,
   },
 })
