8e7aefdf4d49340f7cb35d7ee1739e1f0ef3b28a
raherygino <georgino197@gmail.com>
2023-05-10 18:45:29 +0200

    Debug: todo controller

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -6,7 +6,7 @@ const Todo = require('../models/todoModel')
 const createTodo = asyncHandler(async (req, res) => {
     const todo = await Todo.create({
         user: req.user.id,
-        title: req.body.text
+        title: req.body.title
     })
     res.status(200).json(todo)
 })
