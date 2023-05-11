f51fa0aa5de1a9742573433f69bfbdab74f8d368
raherygino <georgino197@gmail.com>
2023-05-11 05:32:45 +0200

    Delete todo

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -4,6 +4,12 @@ const Todo = require('../models/todoModel')
 
 
 const createTodo = asyncHandler(async (req, res) => {
+
+    if (!req.body.title) {
+        res.status(400)
+        throw new Error('Field title required')
+    }
+
     const todo = await Todo.create({
         user: req.user.id,
         title: req.body.title
@@ -16,11 +22,16 @@ const getTodos = asyncHandler(async (req, res) => {
     const todos = await Todo.find({ user: req.user.id })
   
     res.status(200).json(todos)
-  })
+})
 
-
+const deleteTodo = asyncHandler(async (req, res) => {
+    const todo = await Todo.findById(req.params.id)
+    await todo.remove()
+    res.status(200).json({id: req.params.id})
+})
 
 module.exports = {
     createTodo,
     getTodos,
+    deleteTodo,
 }
--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const {
-  createTodo, getTodos,
+  createTodo, getTodos, deleteTodo,
 } = require('../controllers/todoController')
 
 const { protect } = require('../middleware/authMiddleware')
 
 router.route('/').post(protect, createTodo).get(protect, getTodos)
+router.route('/:id').delete(protect, deleteTodo)
 
 module.exports = router
